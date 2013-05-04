4a08b08cf38d09967066f7fe8271d7b9c2a1a491
huy <huy@huyng.com>
2013-05-03 22:23:47 -0700

    Initial Commit

---
+++ HelloCV/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HelloCV
+//
+//  Created by Huy Nguyen on 5/3/13.
+//  Copyright (c) 2013 HelloCv. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
